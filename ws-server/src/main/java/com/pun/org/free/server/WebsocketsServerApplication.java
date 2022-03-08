557f3f7ce9a08c911e9263770a291cee70c2a72a
Ish Mahajan <ishmahajanbhanu@gmail.com>
2022-03-08 13:26:10 +0530

    add spring cloud dependency

--- ws-server/src/main/java/com/pun/org/free/server/WebsocketsServerApplication.java
+++ ws-server/src/main/java/com/pun/org/free/server/WebsocketsServerApplication.java
@@ -2,7 +2,9 @@ package com.pun.org.free.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class WebsocketsServerApplication {
 
